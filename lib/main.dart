5f235e97e383bda0d751480a91ce680bcfe89aa9
Anh Duy <anhduyg07@gmail.com>
2022-04-07 20:41:47 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:myweather/screens/loading_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      debugShowCheckedModeBanner: false,
+      home: LoadingScreen(),
+    );
+  }
+}
